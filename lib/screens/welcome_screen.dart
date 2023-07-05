a56db06a362d972838ab7e3aecab6a39e8fdab78
aiswarya <aiswaryamohan636@gmail.com>
2023-07-05 13:14:06 +0530

    welcome screen commit

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -19,72 +19,75 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     return Scaffold(
       backgroundColor: Color(0xffe4fbed),
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/image1.png",
-                  height: 300,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "Let's get started",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xff010402), // Set the text color to white
+        child: SingleChildScrollView(
+          // Wrap the content with SingleChildScrollView
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/image1.png",
+                    height: 300,
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  '"The greatest threat to our planet is the belief that someone else will save it." - Robert Swan',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Color(0xff010402), // Set the quote text color
-                    fontWeight: FontWeight.bold,
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  height: 70,
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 16.0),
-                    child: CustomButton(
-                      onPressed: () async {
-                        // When true, fetch shared preference data
-                        if (ap.isSignedIn == true) {
-                          await ap.getDataFromSP().whenComplete(
-                                () => Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => MainScreen(),
-                                  ),
-                                ),
-                              );
-                        } else {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const RegisterScreen(),
-                            ),
-                          );
-                        }
-                      },
-                      text: "Get started",
+                  const Text(
+                    "Let's get started",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff010402), // Set the text color to white
                     ),
                   ),
-                )
-              ],
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    '"The greatest threat to our planet is the belief that someone else will save it." - Robert Swan',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff010402), // Set the quote text color
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 70,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 16.0),
+                      child: CustomButton(
+                        onPressed: () async {
+                          // When true, fetch shared preference data
+                          if (ap.isSignedIn == true) {
+                            await ap.getDataFromSP().whenComplete(
+                                  () => Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => MainScreen(),
+                                    ),
+                                  ),
+                                );
+                          } else {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const RegisterScreen(),
+                              ),
+                            );
+                          }
+                        },
+                        text: "Get started",
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
